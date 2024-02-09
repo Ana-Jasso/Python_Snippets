430b22c8af6deea4ae5d9e17021bfc2f1dcf30dc
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-02-09 11:46:03 -0600

    Inicio el juego BlackJack.

---
+++ BlackJack_Game/blackjack_game.py
@@ -0,0 +1,9 @@
+# Hello, this code is from Dr. Angela Yu's course, which you can find on Udemy: https://www.udemy.com/course/100-days-of-code/
+# This is my version of the program.
+
+# Credits for ASCII Art
+# Drawing: https://ascii.co.uk/art/gavel
+# Text: https://ascii.co.uk/art/auction
+
+# Assuming that we have an infinite deck
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] # The value of 11 can change based on whether the result is above or below 21.
