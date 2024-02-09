80e21794445ddfe72de4b3217525ed17e769d942
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-02-09 15:33:19 -0600

    Cambio en el ASCII Art.

--- BlackJack_Game/blackjack_game.py
+++ BlackJack_Game/blackjack_game.py
@@ -3,20 +3,23 @@
 
 # Credits for ASCII Art
 # Text: https://ascii.co.uk/art/blackjack
+# Draw: https://www.asciiart.eu/miscellaneous/playing-cards
 
 import os
 
-def display_Ascii_text():
-    print('''
-    _     _            _    _            _    
-    | |   | |          | |  (_)          | |   
-    | |__ | | __ _  ___| | ___  __ _  ___| | __
-    | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
-    | |_) | | (_| | (__|   <| | (_| | (__|   < 
-    |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\ 
-                           _/ |                
-                          |__/
-    ''')
+def display_Ascii_art():
+    ascii_art = """
+    _____
+    |A .  | _____                    _     _            _    _            _    
+    | /.\ ||A ^  | _____            | |   | |          | |  (_)          | |
+    |(_._)|| / \ ||A _  | _____     | |__ | | __ _  ___| | ___  __ _  ___| | __
+    |  |  || \ / || ( ) ||A_ _ |    | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+    |____V||  .  ||(_'_)||( v )|    | |_) | | (_| | (__|   <| | (_| | (__|   < 
+           |____V||  |  || \ / |    |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\ 
+                  |____V||  .  |                           _/ |  
+                         |____V|                          |__/
+    """
+    print(ascii_art)
 
 def limpiar_pantalla():
     # Comando para limpiar la pantalla en sistemas basados en Unix (Linux y macOS)
@@ -35,5 +38,5 @@ continuar = True
 
 while continuar ==True:
     limpiar_pantalla()
-    display_Ascii_text()
+    display_Ascii_art()
     imprimir_bienvenida()
