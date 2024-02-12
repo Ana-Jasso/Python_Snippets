7df86611ed7621331ccc8ce3120a8e2c4a730ff0
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-02-12 12:09:50 -0600

    Inicio, juego de adivinar el nombre.

---
+++ Number_Guessing_Game/guess_the_number.py
@@ -0,0 +1,18 @@
+# Hello, this code is from Dr. Angela Yu's course, which you can find on Udemy: https://www.udemy.com/course/100-days-of-code/
+# This is my version of the program.
+
+# Credits for ASCII Art
+# Text: https://patorjk.com/software/taag/#p=display&f=Big&t=Number%20Guessing%20Game 
+
+def print_hangman_word():
+    ascii_art = '''
+    _
+    | |
+    | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __
+    | '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\
+    | | | | (_| | | | | (_| | | | | | | (_| | | | |
+    |_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                        __/ |
+                    |___/
+    '''
+    print(ascii_art)
