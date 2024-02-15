745e2aa6b7b583820a91f3b0ec2656b0fe709ac7
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-02-14 19:52:12 -0600

    Inicio, cargando archivos necesarios para el juego Higher-Lower.

---
+++ Higher_Lower_Game_V1/higher_lower_game.py
@@ -0,0 +1,12 @@
+# Hello, this code is from Dr. Angela Yu's course, which you can find on Udemy: https://www.udemy.com/course/100-days-of-code/
+# This is my version of the program.
+
+# Credits for ASCII Art
+# Text1: https://patorjk.com/software/taag/#p=display&f=Crawford2&t=Higher-Lower%0A%20%20%20%20%20%20Game
+# Text2: https://patorjk.com/software/taag/#p=display&f=Crawford2&t=VS
+
+import ASCII_drawings as draw # Alias para que el nombre sea más corto
+import game_data
+
+print(draw.game_name)
+print(game_data.data[0])
