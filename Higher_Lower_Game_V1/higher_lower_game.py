a8d4e3da97fe90ebf0638b49de72511d9b42b65c
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-02-14 20:07:07 -0600

    Obtención de A y B.

--- Higher_Lower_Game_V1/higher_lower_game.py
+++ Higher_Lower_Game_V1/higher_lower_game.py
@@ -5,9 +5,40 @@
 # Text1: https://patorjk.com/software/taag/#p=display&f=Crawford2&t=Higher-Lower%0A%20%20%20%20%20%20Game
 # Text2: https://patorjk.com/software/taag/#p=display&f=Crawford2&t=VS
 
-from ASCII_drawings import game_name, vs # Alias para que el nombre sea más corto
+import os
+import random
+from ASCII_drawings import game_name, vs
 from game_data import data
 
-print(game_name)
-print(data[0])
-print(vs)
+# print(game_name)
+# print(data[0])
+# print(vs)
+
+def limpiar_pantalla():
+    # Comando para limpiar la pantalla en sistemas basados en Unix (Linux y macOS)
+    if os.name == 'posix':
+        os.system('clear')
+    # Comando para limpiar la pantalla en sistemas basados en Windows
+    elif os.name == 'nt':
+        os.system('cls')
+
+def obtener_dos_elementos_diferentes(lista):
+    elemento_1 = random.choice(lista)
+    elemento_2 = random.choice(lista)
+    
+    while elemento_2 == elemento_1:
+        elemento_2 = random.choice(lista)
+    
+    return elemento_1, elemento_2
+
+continuar = True
+
+while continuar:
+    limpiar_pantalla()
+    print(game_name)
+    A, B = obtener_dos_elementos_diferentes(data)
+    print(A)
+    print(B)
+    #print(f'Compare A: {data['name']}')
+    print(vs)
+    continuar = False
