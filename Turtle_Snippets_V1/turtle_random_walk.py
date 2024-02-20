865270e84de6366272386574c576f0dfbd4315e5
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-02-20 10:24:24 -0600

    Update funciones.

--- Turtle_Snippets_V1/turtle_random_walk.py
+++ Turtle_Snippets_V1/turtle_random_walk.py
@@ -5,23 +5,23 @@ from turtle import *
 import random
 
 COLORS = ['#964083', '#C572A5', '#E38C24', '#EAB8D9', '#B2B9BF', '#709A86']
-GRADOS_PREDETERMINADOS = [0, 90, 180, 270, 360]
+GRADOS_PREDETERMINADOS = [0, 90, 180, 270]
 
 my_turtle = Turtle()
 
 my_turtle.shape("turtle")
 my_turtle.width(11)
-speed = 1
-for i in range(400):
+speed = 5
+for i in range(450):
     color = random.choice(COLORS)
     my_turtle.color(color)
 
     grados = random.choice(GRADOS_PREDETERMINADOS)
-    my_turtle.right(grados)
+    my_turtle.setheading(grados)
 
     my_turtle.forward(20)
-    if i % 10:
-        speed += 5
+    if i % 10 == 0:
+        speed += 20
     my_turtle.speed(speed)
 
 screen = Screen()
