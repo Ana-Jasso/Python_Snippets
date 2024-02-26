94ddb439f42c53cc2742ee50f1465ae1f6e3bad5
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-02-26 16:24:37 -0600

    Inicia el movimiento de la serpiente, el programa comienza a no responder bien con el ciclo.

--- Snake_Game_V1/main.py
+++ Snake_Game_V1/main.py
@@ -1,16 +1,30 @@
 from turtle import *
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
+screen.tracer(0)
 
 STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
-segment = [0,0,0]
+segments = []
 
 for positions in STARTING_POSITIONS:
     segment = Turtle('square')
     segment.color("white")
+    segment.penup()
     segment.goto(positions)
+    segments.append(segment)
+
+screen.update()
+
+game_is_on = True
+while(game_is_on):
+    screen.update()
+    for seg in segments:
+        seg.forward(20)
+        time.sleep(1)
+
 
 screen.exitonclick()
