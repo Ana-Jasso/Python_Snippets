09068a7a97e9aef65625b7c92309ce535e0b04ee
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-02-28 10:38:05 -0600

    Crear la clase serpiente para ajustar a POO.

---
+++ Snake_Game_V1/snake.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+class Snake:
+    def __init__(self):
+        STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+        self.segments = []
+        for positions in STARTING_POSITIONS:
+            segment = Turtle('square')
+            segment.color("white")
+            segment.penup()
+            segment.goto(positions)
+            self.segments.append(segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments)-1,0,-1):
+            new_x = self.segments[seg_num-1].xcor()
+            new_y = self.segments[seg_num-1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
+        self.segments[0].left(90)
+
