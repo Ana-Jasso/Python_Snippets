1b5d76c985fc39d0e9332bcab61ac69be1b6689a
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-03-18 11:16:50 -0600

    Creando la screen (pantalla) para Pong.

---
+++ Pong_Game_V1/main.py
@@ -0,0 +1,11 @@
+# Hello, this code is from Dr. Angela Yu's course, which you can find on Udemy: https://www.udemy.com/course/100-days-of-code/
+# This is my version of the program.
+
+from turtle import *
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.title('Pong')
+
+screen.exitonclick()
