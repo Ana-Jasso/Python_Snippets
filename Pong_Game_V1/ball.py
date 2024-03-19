b527eed043e75ffc66e91e8364a40f39c4195520
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-03-19 11:02:36 -0600

    Pong terminado.

--- Pong_Game_V1/ball.py
+++ Pong_Game_V1/ball.py
@@ -2,15 +2,31 @@ from turtle import *
 
 COLOR = 'blue'
 SHAPE = 'circle'
-STCH_WID = 1
-STCH_LEN = 1
 
 class Ball(Turtle):
-    def __init__(self, x_pos, y_pos):
+
+    def __init__(self):
         super().__init__()
-        self.color(COLOR)
-        self.shape(SHAPE)
-        #self.shapesize(stretch_wid=, stretch_len=)
+        self.color("white")
+        self.shape("circle")
         self.penup()
-        self.goto(x_pos,y_pos)
-    pass
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- Pong_Game_V1/main.py
+++ Pong_Game_V1/main.py
@@ -4,6 +4,7 @@
 from turtle import *
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -14,7 +15,8 @@ screen.tracer(0)
 
 right_paddle = Paddle(x_pos=350, y_pos=0)
 left_paddle = Paddle(x_pos=-350, y_pos=0)
-ball = Ball(x_pos=0, y_pos=0)
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(right_paddle.move_up, 'Up')
@@ -24,6 +26,18 @@ screen.onkeypress(left_paddle.move_down, 's')
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.01)
     screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
