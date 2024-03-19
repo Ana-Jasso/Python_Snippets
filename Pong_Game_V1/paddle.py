778a3c9837cf793c2ecf0bb34e155c4f4574e553
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-03-19 10:14:28 -0600

    Configuración de variables.

--- Pong_Game_V1/paddle.py
+++ Pong_Game_V1/paddle.py
@@ -2,20 +2,23 @@ from turtle import *
 
 COLOR = 'white'
 SHAPE = 'square'
+STEPS = 15
+STCH_WID = 5
+STCH_LEN = 1
 
 class Paddle(Turtle):
     def __init__(self, x_pos, y_pos):
         super().__init__()
         self.color(COLOR)
         self.shape(SHAPE)
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.shapesize(stretch_wid=STCH_WID, stretch_len=STCH_LEN)
         self.penup()
         self.goto(x_pos,y_pos)
     
     def move_up(self):
-        new_y = self.ycor() + 15
+        new_y = self.ycor() + STEPS
         self.goto(self.xcor(), new_y)
 
     def move_down(self):
-        new_y = self.ycor() - 15
+        new_y = self.ycor() - STEPS
         self.goto(self.xcor(), new_y)
