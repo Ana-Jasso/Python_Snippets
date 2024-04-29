e9145c537d21553c6fc520d65dbfecc8dba54a63
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-04-29 09:48:46 -0600

    Inicio Password Manager V1.

---
+++ Password_Manager_V1/main.py
@@ -0,0 +1,8 @@
+# Hello, this code is from Dr. Angela Yu's course, which you can find on Udemy: https://www.udemy.com/course/100-days-of-code/
+# This is my version of the program.
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
