6bbd86222f953bd25dc9f477a506e20ae6503613
Ana-Jasso <ana.jassocs@uanl.edu.mx>
2024-04-30 11:23:40 -0600

    Añadir función al botón "Add".

--- Password_Manager_V1/main.py
+++ Password_Manager_V1/main.py
@@ -7,6 +7,17 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save_data():
+    website = entry_website.get()
+    email_username = entry_email_username.get()
+    password = entry_password.get()
+    if website.strip() and email_username.strip() and password.strip():
+        empty_entries_warning.config(text='')
+        with open('Password_Manager_V1\data.txt', mode='a') as data:
+            data.write(f'{website} {email_username} {password}\n')
+    else:
+        empty_entries_warning.config(text='Please fill all the fields.')
+
 # ---------------------------- UI SETUP ------------------------------- #
  
 window = Tk()
@@ -27,8 +38,9 @@ entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")
 label_email_uname = Label(text="Email/Username:")
 label_email_uname.grid(column=0, row=2)
  
-entry_email_uname = Entry()
-entry_email_uname.grid(column=1, row=2, columnspan=2, sticky="EW")
+entry_email_username = Entry()
+entry_email_username.grid(column=1, row=2, columnspan=2, sticky="EW")
+#entry_email_username.insert(0, 'email@gmail.com')
  
 label_password = Label(text="Password:")
 label_password.grid(column=0, row=3)
@@ -39,7 +51,10 @@ entry_password.grid(column=1, row=3, sticky="EW")
 generate_btn = Button(text="Generate Password")
 generate_btn.grid(column=2, row=3, sticky="EW")
  
-add_btn = Button(text="Add", width=35)
+add_btn = Button(text="Add", width=35, command=save_data)
 add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
- 
+
+empty_entries_warning = Label()
+empty_entries_warning.grid(column=1, row=5)
+
 mainloop()
